52651db4c8443e73c5252adb0b858ff4bf4c5c29
Eduardo Spada <duspada@users.noreply.github.com>
2022-03-18 15:16:28 -0300

    Update homebrew.sh

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -6,7 +6,7 @@
 brew tap AdoptOpenJDK/openjdk
 
 # Install CLI
-brew install node@16 yarn cocoapods watchman android-platform-tools
+brew install node@17 yarn cocoapods watchman android-platform-tools
 
 # Install Apps
 brew install --cask notion slack visual-studio-code postman android-studio reactotron adoptopenjdk8
